5c720c2c6c5089d4387146e26dead1b755b15b32
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-23 01:12:02 +0900

    Create 2, テーブルの新規作成、参照.sql

---
+++ study_records/2, テーブルの新規作成、参照.sql
@@ -0,0 +1,33 @@
+-- USE（データベース名）で操作対象のデータベースを指定
+USE sample_db;
+-- 結果
+Database changed
+
+-- CREATE TABLE （テーブル名）（カラム名 データ型, , ,）でテーブルを新規作成
+CREATE TABLE test_scores (id INT, name VARCHAR(100),english VARCHAR(100), math VARCHAR(100), science VARCHAR(100));
+-- 結果
+Query OK, 0 rows affected (0.08 sec)
+
+-- SHOW TABLESでテーブル一覧を参照
+SHOW TABLES;
+-- 結果
++---------------------+
+| Tables_in_sample_db |
++---------------------+
+| test_scores         |
++---------------------+
+1 row in set (0.01 sec)
+
+-- DESCRIBE（テーブル名）でテーブルの構造を参照
+DESCRIBE test_scores;
+-- 結果
++---------+--------------+------+-----+---------+-------+
+| Field   | Type         | Null | Key | Default | Extra |
++---------+--------------+------+-----+---------+-------+
+| id      | int(11)      | YES  |     | NULL    |       |
+| name    | varchar(100) | YES  |     | NULL    |       |
+| english | varchar(100) | YES  |     | NULL    |       |
+| math    | varchar(100) | YES  |     | NULL    |       |
+| science | varchar(100) | YES  |     | NULL    |       |
++---------+--------------+------+-----+---------+-------+
+5 rows in set (0.02 sec)
