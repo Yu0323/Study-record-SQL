bae59682d425141237d01e994b57bd5fa934562b
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-22 23:55:07 +0900

    Update study_record.sql

--- study_record.sql
+++ study_record.sql
@@ -8,3 +8,41 @@ mysql> SHOW DATABASES;
 | sys                |
 +--------------------+
 4 rows in set (0.24 sec)
+
+mysql> SHOW DATABASES LIKE '%schema';
++--------------------+
+| Database (%schema) |
++--------------------+
+| information_schema |
+| performance_schema |
++--------------------+
+2 rows in set (0.01 sec)
+
+mysql> CREATE DATABASE sample_db;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sample_db          |
+| sys                |
++--------------------+
+5 rows in set (0.00 sec)
+
+mysql> DROP DATABASE sample_db;
+Query OK, 0 rows affected (0.09 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+4 rows in set (0.00 sec)
