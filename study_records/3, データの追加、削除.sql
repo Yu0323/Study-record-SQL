b82e7ad37f951d955ceca6fb74b423100f3c79c5
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-23 01:14:46 +0900

    Update 3, データの追加、削除.sql

--- study_records/3, データの追加、削除.sql
+++ study_records/3, データの追加、削除.sql
@@ -1,5 +1,5 @@
 -- INSERT INTO（テーブル名）（カラム名,,,）VALUES（要素,,,）でテーブルにデータを追加する
-INSERT INTO test_scores (id,name,english,math,science) VALUES (1,'Yu',98,97,65);
+mysql> INSERT INTO test_scores (id,name,english,math,science) VALUES (1,'Yu',98,97,65);
 Query OK, 1 row affected (0.02 sec)
 +---------+--------------+------+-----+---------+-------+
 | Field   | Type         | Null | Key | Default | Extra |
