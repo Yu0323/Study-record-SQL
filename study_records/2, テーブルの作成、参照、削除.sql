d8b7435a453bdc480ac21f6d101758ec36eaeaed
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-23 01:23:54 +0900

    Update and rename 2, テーブルの新規作成、参照.sql to 2, テーブルの作成、参照、削除.sql

--- study_records/2, テーブルの作成、参照、削除.sql
+++ study_records/2, テーブルの作成、参照、削除.sql
@@ -3,7 +3,7 @@ mysql> USE sample_db;
 -- 結果
 Database changed
 
--- CREATE TABLE （テーブル名）（カラム名 データ型, , ,）でテーブルを新規作成
+-- CREATE TABLE （テーブル名）（カラム名 データ型, , ,）でテーブルを作成
 mysql> CREATE TABLE test_scores (id INT, name VARCHAR(100),english VARCHAR(100), math VARCHAR(100), science VARCHAR(100));
 -- 結果
 Query OK, 0 rows affected (0.08 sec)
@@ -31,3 +31,11 @@ mysql> DESCRIBE test_scores;
 | science | varchar(100) | YES  |     | NULL    |       |
 +---------+--------------+------+-----+---------+-------+
 5 rows in set (0.02 sec)
+
+-- DROP（テーブル名）でテーブルを削除
+mysql> DROP test_scores;
+-- 結果
++---------------------+
+| Tables_in_sample_db |
++---------------------+
+0 row in set (0.01 sec)
