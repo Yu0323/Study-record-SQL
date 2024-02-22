94c101f9973bb9eb054855ba0e23c56d3f09b9a3
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-22 23:48:01 +0900

    Update study_record.sql

--- study_record.sql
+++ study_record.sql
@@ -1,4 +1,10 @@
---テーブル"tableA"から，"apple"行のデータを取り出す
-
-SELECT apple
-FROM tableA
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+4 rows in set (0.24 sec)
