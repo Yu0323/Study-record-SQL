a9aa32c29b871b286fec6aef4ed152b4030656d9
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-24 02:38:13 +0900

    Update 9, 指定した範囲に値が含まれるか否かの判定.sql

--- study_records/9, 指定した範囲に値が含まれるか否かの判定.sql
+++ study_records/9, 指定した範囲に値が含まれるか否かの判定.sql
@@ -1,4 +1,19 @@
-mysql> SELECT * FROM test_scores WHERE math BETWEEN 80 AND 90;
+/*
+<test_scores 拡張版>
++------+--------+---------+------+---------+
+| id   | name   | english | math | science |
++------+--------+---------+------+---------+
+|    1 | Yu     | 98      | 97   | 65      |
+|    2 | Ai     | 96      | 80   | 76      |
+|    3 | Ken    | 90      | 67   | 78      |
+|    4 |        | 67      | 89   | 90      |
+|    5 | Ishida | 78      | 98   | NULL    |
++------+--------+---------+------+---------+
+*/
+-- BETWEEN句を用いた、特定の範囲に値が含まれるデータの取得
+SELECT * FROM test_scores WHERE math BETWEEN 80 AND 90;
+-- 結果
+/*
 +------+------+---------+------+---------+
 | id   | name | english | math | science |
 +------+------+---------+------+---------+
@@ -6,8 +21,12 @@ mysql> SELECT * FROM test_scores WHERE math BETWEEN 80 AND 90;
 |    4 |      | 67      | 89   | 90      |
 +------+------+---------+------+---------+
 2 rows in set (0.00 sec)
+*/
 
-mysql> SELECT * FROM test_scores WHERE math NOT BETWEEN 80 AND 90;
+-- NOT句をBETWEENの前に付けると、特定の範囲に値が含まれないデータを取得する
+SELECT * FROM test_scores WHERE math NOT BETWEEN 80 AND 90;
+-- 結果
+/*
 +------+--------+---------+------+---------+
 | id   | name   | english | math | science |
 +------+--------+---------+------+---------+
@@ -16,3 +35,4 @@ mysql> SELECT * FROM test_scores WHERE math NOT BETWEEN 80 AND 90;
 |    5 | Ishida | 78      | 98   | NULL    |
 +------+--------+---------+------+---------+
 3 rows in set (0.00 sec)
+*/
