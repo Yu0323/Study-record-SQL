1a6a330507b35ef53749574c6fa5092237574ae9
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-24 02:10:00 +0900

    Update 2, テーブルの作成、参照、削除.sql

--- study_records/2, テーブルの作成、参照、削除.sql
+++ study_records/2, テーブルの作成、参照、削除.sql
@@ -1,26 +1,34 @@
 -- USE（データベース名）で操作対象のデータベースを指定
-mysql> USE sample_db;
+USE sample_db;
 -- 結果
+/*
 Database changed
+*/
 
 -- CREATE TABLE （テーブル名）（カラム名 データ型, , ,）でテーブルを作成
-mysql> CREATE TABLE test_scores (id INT, name VARCHAR(100),english VARCHAR(100), math VARCHAR(100), science VARCHAR(100));
+CREATE TABLE test_scores (id INT, name VARCHAR(100),english VARCHAR(100), math VARCHAR(100), science VARCHAR(100));
 -- 結果
+/*
 Query OK, 0 rows affected (0.08 sec)
+※適当なデータを入力した
+*/
 
 -- SHOW TABLESでテーブル一覧を参照
 SHOW TABLES;
 -- 結果
+/*
 +---------------------+
 | Tables_in_sample_db |
 +---------------------+
 | test_scores         |
 +---------------------+
 1 row in set (0.01 sec)
+*/
 
 -- DESCRIBE（テーブル名）でテーブルの構造を参照
-mysql> DESCRIBE test_scores;
+DESCRIBE test_scores;
 -- 結果
+/*
 +---------+--------------+------+-----+---------+-------+
 | Field   | Type         | Null | Key | Default | Extra |
 +---------+--------------+------+-----+---------+-------+
@@ -31,11 +39,14 @@ mysql> DESCRIBE test_scores;
 | science | varchar(100) | YES  |     | NULL    |       |
 +---------+--------------+------+-----+---------+-------+
 5 rows in set (0.02 sec)
+*/
 
 -- DROP（テーブル名）でテーブルを削除
-mysql> DROP test_scores;
+DROP test_scores;
 -- 結果
+/*
 +---------------------+
 | Tables_in_sample_db |
 +---------------------+
 0 row in set (0.01 sec)
+*/
