c4d109e0291701b0330b09e7bbe23f7d4a62c7c4
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-24 02:21:35 +0900

    Update 6, NULLの判定.sql

--- study_records/6, NULLの判定.sql
+++ study_records/6, NULLの判定.sql
@@ -1,3 +1,4 @@
+/*
 <test_scores 拡張版>
 +------+--------+---------+------+---------+
 | id   | name   | english | math | science |
@@ -8,26 +9,32 @@
 |    4 |        | 67      | 89   | 90      |
 |    5 | Ishida | 78      | 98   | NULL    |
 +------+--------+---------+------+---------+
+*/
 
 -- scienceカラムがNULLであるものを選択
-mysql> SELECT * FROM test_scores WHERE science IS NULL;
+SELECT * FROM test_scores WHERE science IS NULL;
 -- 結果
+/*
 +------+--------+---------+------+---------+
 | id   | name   | english | math | science |
 +------+--------+---------+------+---------+
 |    5 | Ishida | 78      | 98   | NULL    |
 +------+--------+---------+------+---------+
 1 row in set (0.01 sec)
+*/
 
 -- nameカラムがNULLであるものを選択
-mysql> SELECT * FROM test_scores WHERE name IS NULL;
+SELECT * FROM test_scores WHERE name IS NULL;
 -- 結果
 -- 空白文字' 'とNULLは区別されることに注意！
+/*
 Empty set (0.00 sec)
+*/
 
 -- scienceカラムがNULLでないものを選択
-mysql> SELECT * FROM test_scores WHERE science IS NOT NULL;
+SELECT * FROM test_scores WHERE science IS NOT NULL;
 -- 結果
+/*
 +------+------+---------+------+---------+
 | id   | name | english | math | science |
 +------+------+---------+------+---------+
@@ -37,3 +44,4 @@ mysql> SELECT * FROM test_scores WHERE science IS NOT NULL;
 |    4 |      | 67      | 89   | 90      |
 +------+------+---------+------+---------+
 4 rows in set (0.00 sec)
+*/
