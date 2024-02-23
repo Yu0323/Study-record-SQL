d235a871089c27078315fd6fb042c71d63290d09
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-24 03:26:18 +0900

    Update 3-2, 取得するデータ数の制限.sql

--- study_records/3-2, 取得するデータ数の制限.sql
+++ study_records/3-2, 取得するデータ数の制限.sql
@@ -1,4 +1,21 @@
-mysql> SELECT * FROM test_scores LIMIT 2;
+/*
+<test_scores 拡張版>
++------+---------+---------+------+---------+
+| id   | name    | english | math | science |
++------+---------+---------+------+---------+
+|    1 | Yu      | 98      | 97   | 65      |
+|    2 | Ai      | 96      | 80   | 76      |
+|    3 | Ken     | 90      | 67   | 78      |
+|    4 |         | 67      | 89   | 90      |
+|    5 | Ishida  | 78      | 98   | NULL    |
+|    6 | Miyoshi | 96      | 84   | 53      |
++------+---------+---------+------+---------+
+*/
+
+-- 末尾にLIMIT句を付けることで取得するデータの数を制限
+SELECT * FROM test_scores LIMIT 2;
+-- 結果
+/*
 +------+------+---------+------+---------+
 | id   | name | english | math | science |
 +------+------+---------+------+---------+
@@ -6,10 +23,12 @@ mysql> SELECT * FROM test_scores LIMIT 2;
 |    2 | Ai   | 96      | 80   | 76      |
 +------+------+---------+------+---------+
 2 rows in set (0.01 sec)
+*/
 
-mysql> SELECT * FROM test_scores OFFSET 2 LIMIT 3;
-ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '2 LIMIT 3' at line 1
-mysql> SELECT * FROM test_scores LIMIT 3 OFFSET 2;
+-- 末尾にLIMIT句とOFFSET句を付けることで、起点の位置から数えて、取得するデータの数を制限
+SELECT * FROM test_scores LIMIT 3 OFFSET 2;
+-- 結果
+/*
 +------+--------+---------+------+---------+
 | id   | name   | english | math | science |
 +------+--------+---------+------+---------+
@@ -18,8 +37,12 @@ mysql> SELECT * FROM test_scores LIMIT 3 OFFSET 2;
 |    5 | Ishida | 78      | 98   | NULL    |
 +------+--------+---------+------+---------+
 3 rows in set (0.00 sec)
+*/
 
-mysql> SELECT * FROM test_scores ORDER BY science LIMIT 4;
+-- ORDER BYによるソートの結果に対して、LIMIT句を適用
+SELECT * FROM test_scores ORDER BY science LIMIT 4;
+-- 結果
+/*
 +------+---------+---------+------+---------+
 | id   | name    | english | math | science |
 +------+---------+---------+------+---------+
@@ -29,3 +52,4 @@ mysql> SELECT * FROM test_scores ORDER BY science LIMIT 4;
 |    2 | Ai      | 96      | 80   | 76      |
 +------+---------+---------+------+---------+
 4 rows in set (0.01 sec)
+*/
