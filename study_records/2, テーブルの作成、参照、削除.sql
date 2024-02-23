97892872a81bbaad42e7f280f0de8aa42f835798
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-24 02:10:57 +0900

    Update 2, テーブルの作成、参照、削除.sql

--- study_records/2, テーブルの作成、参照、削除.sql
+++ study_records/2, テーブルの作成、参照、削除.sql
@@ -10,7 +10,7 @@ CREATE TABLE test_scores (id INT, name VARCHAR(100),english VARCHAR(100), math V
 -- 結果
 /*
 Query OK, 0 rows affected (0.08 sec)
-※適当なデータを入力した
+※適当なカラム名とデータ型を指定した
 */
 
 -- SHOW TABLESでテーブル一覧を参照
