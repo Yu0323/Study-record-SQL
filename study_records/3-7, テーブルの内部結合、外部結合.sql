77b8e4f5fe52cbcc5a48a6c12b8291567499a0ff
Yu0323 <153826259+Yu0323@users.noreply.github.com>
2024-02-26 00:31:14 +0900

    Create 3-7, テーブルの内部結合、外部結合.sql

---
+++ study_records/3-7, テーブルの内部結合、外部結合.sql
@@ -0,0 +1,12 @@
++---------+--------+
+| name    | gender |
++---------+--------+
+| Yu      | man    |
+| Ai      | woman  |
+| Sato    | woman  |
+| Ishida  | woman  |
+| Miyoshi | woman  |
+| Maeda   | man    |
+| Yoshida | man    |
+| Tanaka  | man    |
++---------+--------+
